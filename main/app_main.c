6b75319bfee4162c221f90f424d0ec5b992cd722
kelvinyhenrique <kelvinyhenrique17@gmail.com>
2023-04-05 23:04:11 -0300

    Change node name

--- main/app_main.c
+++ main/app_main.c
@@ -36,107 +36,122 @@ esp_rmaker_device_t *switch_device;
 
 /* Callback to handle commands received from the RainMaker cloud */
 static esp_err_t write_cb(const esp_rmaker_device_t *device, const esp_rmaker_param_t *param,
-            const esp_rmaker_param_val_t val, void *priv_data, esp_rmaker_write_ctx_t *ctx)
+                          const esp_rmaker_param_val_t val, void *priv_data, esp_rmaker_write_ctx_t *ctx)
 {
-    if (ctx) {
+    if (ctx)
+    {
         ESP_LOGI(TAG, "Received write request via : %s", esp_rmaker_device_cb_src_to_str(ctx->src));
     }
-    if (strcmp(esp_rmaker_param_get_name(param), ESP_RMAKER_DEF_POWER_NAME) == 0) {
+    if (strcmp(esp_rmaker_param_get_name(param), ESP_RMAKER_DEF_POWER_NAME) == 0)
+    {
         ESP_LOGI(TAG, "Received value = %s for %s - %s",
-                val.val.b? "true" : "false", esp_rmaker_device_get_name(device),
-                esp_rmaker_param_get_name(param));
+                 val.val.b ? "true" : "false", esp_rmaker_device_get_name(device),
+                 esp_rmaker_param_get_name(param));
         app_driver_set_state(val.val.b);
         esp_rmaker_param_update_and_report(param, val);
     }
     return ESP_OK;
 }
 /* Event handler for catching RainMaker events */
-static void event_handler(void* arg, esp_event_base_t event_base,
-                          int32_t event_id, void* event_data)
+static void event_handler(void *arg, esp_event_base_t event_base,
+                          int32_t event_id, void *event_data)
 {
-    if (event_base == RMAKER_EVENT) {
-        switch (event_id) {
-            case RMAKER_EVENT_INIT_DONE:
-                ESP_LOGI(TAG, "RainMaker Initialised.");
-                break;
-            case RMAKER_EVENT_CLAIM_STARTED:
-                ESP_LOGI(TAG, "RainMaker Claim Started.");
-                break;
-            case RMAKER_EVENT_CLAIM_SUCCESSFUL:
-                ESP_LOGI(TAG, "RainMaker Claim Successful.");
-                break;
-            case RMAKER_EVENT_CLAIM_FAILED:
-                ESP_LOGI(TAG, "RainMaker Claim Failed.");
-                break;
-            default:
-                ESP_LOGW(TAG, "Unhandled RainMaker Event: %"PRIi32, event_id);
+    if (event_base == RMAKER_EVENT)
+    {
+        switch (event_id)
+        {
+        case RMAKER_EVENT_INIT_DONE:
+            ESP_LOGI(TAG, "RainMaker Initialised.");
+            break;
+        case RMAKER_EVENT_CLAIM_STARTED:
+            ESP_LOGI(TAG, "RainMaker Claim Started.");
+            break;
+        case RMAKER_EVENT_CLAIM_SUCCESSFUL:
+            ESP_LOGI(TAG, "RainMaker Claim Successful.");
+            break;
+        case RMAKER_EVENT_CLAIM_FAILED:
+            ESP_LOGI(TAG, "RainMaker Claim Failed.");
+            break;
+        default:
+            ESP_LOGW(TAG, "Unhandled RainMaker Event: %" PRIi32, event_id);
         }
-    } else if (event_base == RMAKER_COMMON_EVENT) {
-        switch (event_id) {
-            case RMAKER_EVENT_REBOOT:
-                ESP_LOGI(TAG, "Rebooting in %d seconds.", *((uint8_t *)event_data));
-                break;
-            case RMAKER_EVENT_WIFI_RESET:
-                ESP_LOGI(TAG, "Wi-Fi credentials reset.");
-                break;
-            case RMAKER_EVENT_FACTORY_RESET:
-                ESP_LOGI(TAG, "Node reset to factory defaults.");
-                break;
-            case RMAKER_MQTT_EVENT_CONNECTED:
-                ESP_LOGI(TAG, "MQTT Connected.");
-                break;
-            case RMAKER_MQTT_EVENT_DISCONNECTED:
-                ESP_LOGI(TAG, "MQTT Disconnected.");
-                break;
-            case RMAKER_MQTT_EVENT_PUBLISHED:
-                ESP_LOGI(TAG, "MQTT Published. Msg id: %d.", *((int *)event_data));
-                break;
-            default:
-                ESP_LOGW(TAG, "Unhandled RainMaker Common Event: %"PRIi32, event_id);
+    }
+    else if (event_base == RMAKER_COMMON_EVENT)
+    {
+        switch (event_id)
+        {
+        case RMAKER_EVENT_REBOOT:
+            ESP_LOGI(TAG, "Rebooting in %d seconds.", *((uint8_t *)event_data));
+            break;
+        case RMAKER_EVENT_WIFI_RESET:
+            ESP_LOGI(TAG, "Wi-Fi credentials reset.");
+            break;
+        case RMAKER_EVENT_FACTORY_RESET:
+            ESP_LOGI(TAG, "Node reset to factory defaults.");
+            break;
+        case RMAKER_MQTT_EVENT_CONNECTED:
+            ESP_LOGI(TAG, "MQTT Connected.");
+            break;
+        case RMAKER_MQTT_EVENT_DISCONNECTED:
+            ESP_LOGI(TAG, "MQTT Disconnected.");
+            break;
+        case RMAKER_MQTT_EVENT_PUBLISHED:
+            ESP_LOGI(TAG, "MQTT Published. Msg id: %d.", *((int *)event_data));
+            break;
+        default:
+            ESP_LOGW(TAG, "Unhandled RainMaker Common Event: %" PRIi32, event_id);
         }
-    } else if (event_base == APP_WIFI_EVENT) {
-        switch (event_id) {
-            case APP_WIFI_EVENT_QR_DISPLAY:
-                ESP_LOGI(TAG, "Provisioning QR : %s", (char *)event_data);
-                break;
-            case APP_WIFI_EVENT_PROV_TIMEOUT:
-                ESP_LOGI(TAG, "Provisioning Timed Out. Please reboot.");
-                break;
-            case APP_WIFI_EVENT_PROV_RESTART:
-                ESP_LOGI(TAG, "Provisioning has restarted due to failures.");
-                break;
-            default:
-                ESP_LOGW(TAG, "Unhandled App Wi-Fi Event: %"PRIi32, event_id);
-                break;
+    }
+    else if (event_base == APP_WIFI_EVENT)
+    {
+        switch (event_id)
+        {
+        case APP_WIFI_EVENT_QR_DISPLAY:
+            ESP_LOGI(TAG, "Provisioning QR : %s", (char *)event_data);
+            break;
+        case APP_WIFI_EVENT_PROV_TIMEOUT:
+            ESP_LOGI(TAG, "Provisioning Timed Out. Please reboot.");
+            break;
+        case APP_WIFI_EVENT_PROV_RESTART:
+            ESP_LOGI(TAG, "Provisioning has restarted due to failures.");
+            break;
+        default:
+            ESP_LOGW(TAG, "Unhandled App Wi-Fi Event: %" PRIi32, event_id);
+            break;
         }
-    } else if (event_base == RMAKER_OTA_EVENT) {
-        switch(event_id) {
-            case RMAKER_OTA_EVENT_STARTING:
-                ESP_LOGI(TAG, "Starting OTA.");
-                break;
-            case RMAKER_OTA_EVENT_IN_PROGRESS:
-                ESP_LOGI(TAG, "OTA is in progress.");
-                break;
-            case RMAKER_OTA_EVENT_SUCCESSFUL:
-                ESP_LOGI(TAG, "OTA successful.");
-                break;
-            case RMAKER_OTA_EVENT_FAILED:
-                ESP_LOGI(TAG, "OTA Failed.");
-                break;
-            case RMAKER_OTA_EVENT_REJECTED:
-                ESP_LOGI(TAG, "OTA Rejected.");
-                break;
-            case RMAKER_OTA_EVENT_DELAYED:
-                ESP_LOGI(TAG, "OTA Delayed.");
-                break;
-            case RMAKER_OTA_EVENT_REQ_FOR_REBOOT:
-                ESP_LOGI(TAG, "Firmware image downloaded. Please reboot your device to apply the upgrade.");
-                break;
-            default:
-                ESP_LOGW(TAG, "Unhandled OTA Event: %"PRIi32, event_id);
-                break;
+    }
+    else if (event_base == RMAKER_OTA_EVENT)
+    {
+        switch (event_id)
+        {
+        case RMAKER_OTA_EVENT_STARTING:
+            ESP_LOGI(TAG, "Starting OTA.");
+            break;
+        case RMAKER_OTA_EVENT_IN_PROGRESS:
+            ESP_LOGI(TAG, "OTA is in progress.");
+            break;
+        case RMAKER_OTA_EVENT_SUCCESSFUL:
+            ESP_LOGI(TAG, "OTA successful.");
+            break;
+        case RMAKER_OTA_EVENT_FAILED:
+            ESP_LOGI(TAG, "OTA Failed.");
+            break;
+        case RMAKER_OTA_EVENT_REJECTED:
+            ESP_LOGI(TAG, "OTA Rejected.");
+            break;
+        case RMAKER_OTA_EVENT_DELAYED:
+            ESP_LOGI(TAG, "OTA Delayed.");
+            break;
+        case RMAKER_OTA_EVENT_REQ_FOR_REBOOT:
+            ESP_LOGI(TAG, "Firmware image downloaded. Please reboot your device to apply the upgrade.");
+            break;
+        default:
+            ESP_LOGW(TAG, "Unhandled OTA Event: %" PRIi32, event_id);
+            break;
         }
-    } else {
+    }
+    else
+    {
         ESP_LOGW(TAG, "Invalid event received!");
     }
 }
@@ -152,11 +167,12 @@ void app_main()
 
     /* Initialize NVS. */
     esp_err_t err = nvs_flash_init();
-    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
         ESP_ERROR_CHECK(nvs_flash_erase());
         err = nvs_flash_init();
     }
-    ESP_ERROR_CHECK( err );
+    ESP_ERROR_CHECK(err);
 
     /* Initialize Wi-Fi. Note that, this should be called before esp_rmaker_node_init()
      */
@@ -174,10 +190,11 @@ void app_main()
     esp_rmaker_config_t rainmaker_cfg = {
         .enable_time_sync = false,
     };
-    esp_rmaker_node_t *node = esp_rmaker_node_init(&rainmaker_cfg, "ESP RainMaker Device", "Switch");
-    if (!node) {
+    esp_rmaker_node_t *node = esp_rmaker_node_init(&rainmaker_cfg, "Smart Pump Device", "Switch");
+    if (!node)
+    {
         ESP_LOGE(TAG, "Could not initialise node. Aborting!!!");
-        vTaskDelay(5000/portTICK_PERIOD_MS);
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
         abort();
     }
 
@@ -240,9 +257,10 @@ void app_main()
      * after a connection has been successfully established
      */
     err = app_wifi_start(POP_TYPE_RANDOM);
-    if (err != ESP_OK) {
+    if (err != ESP_OK)
+    {
         ESP_LOGE(TAG, "Could not start Wifi. Aborting!!!");
-        vTaskDelay(5000/portTICK_PERIOD_MS);
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
         abort();
     }
 }
